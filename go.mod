f051d4a5cf0e06a4aa339687f87f5f40415e0a87
Rasmus Schlünsen <rasmus@schlunsen.com>
2021-02-20 14:29:58 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/schlunsen/go-portscanner
+
+go 1.15
+
+require (
+	github.com/go-acme/lego v2.7.2+incompatible
+	github.com/go-acme/lego/v3 v3.9.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.1.3
+	github.com/spf13/viper v1.7.1
+)
---
+++ internal/scanner/scanner.go
@@ -0,0 +1,72 @@
+package scanner
+
+import (
+	"fmt"
+	"net"
+	"sort"
+	"time"
+)
+
+type Scanner struct {
+	Hostname  string
+	Startport int16
+	Endport   int16
+}
+
+func NewScanner(hostname string, startport int16, endport int16) *Scanner {
+	s := new(Scanner)
+	s.Hostname = hostname
+	s.Startport = startport
+	s.Endport = endport
+	return s
+}
+
+func worker(hostname string, ports, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("%s:%d", hostname, p)
+		conn, err := net.DialTimeout("tcp", address, 1*time.Second)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
+	}
+}
+
+func (s Scanner) Scan() {
+	ports := make(chan int, 100)
+	// this channel will receive results of scanning
+	results := make(chan int)
+	var openports []int
+
+	// create a pool of workers
+	for i := 0; i < cap(ports); i++ {
+		go worker(s.Hostname, ports, results)
+	}
+
+	// send ports to be scanned
+	go func() {
+		for i := int(s.Startport); i <= int(s.Endport); i++ {
+			ports <- i
+		}
+	}()
+
+	for i := s.Startport; i <= s.Endport; i++ {
+		port := <-results
+		if port != 0 {
+			fmt.Printf("%d open\n", port)
+			openports = append(openports, port)
+		}
+	}
+
+	// After all the work has been completed, close the channels
+	close(ports)
+	close(results)
+	// sort open port numbers
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
+	}
+
+}
